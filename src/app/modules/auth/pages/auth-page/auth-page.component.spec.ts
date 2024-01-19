cc0a9173eca7f053248882848a6967e9c1fb73d8
Veronidas <veronabrkich@gmail.com>
2024-01-19 16:42:10 -0300

    Component and services tests

--- src/app/modules/auth/pages/auth-page/auth-page.component.spec.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { AuthPageComponent } from './auth-page.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 describe('AuthPageComponent', () => {
   let component: AuthPageComponent;
@@ -21,7 +22,51 @@ describe('AuthPageComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  //AAA:
+  //Arrange, arrancar
+  //Act
+  //Assert, verificar
+
+  it('should create component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Form should return invalid', () => {
+
+    const mockCredentials = {
+      email: '0x0x0x0x',
+      password: '111111111111111111111111111111'
+    }
+  
+    const emailForm: any = component.formLogin.get('email')
+    const passwordForm: any = component.formLogin.get('password')
+
+    emailForm.setValue(mockCredentials.email)
+    passwordForm.setValue(mockCredentials.password)
+
+    expect(component.formLogin.invalid).toEqual(true);
+  });
+
+  it('Form should return valid', () => {
+
+    const mockCredentials = {
+      email: 'test@test.com',
+      password: '12345678'
+    }
+  
+    const emailForm: any = component.formLogin.get('email')
+    const passwordForm: any = component.formLogin.get('password')
+
+    emailForm.setValue(mockCredentials.email)
+    passwordForm.setValue(mockCredentials.password)
+
+    expect(component.formLogin.valid).toEqual(true);
+  });
+
+  it('Button should say "Iniciar sesión"', () => {
+    const elementRef = fixture.debugElement.query(By.css('.form-action button')).nativeElement
+    const getInnerText = elementRef.innerText
+    expect(getInnerText).toEqual('Iniciar sesión')
+  })
+
 });
--- src/app/modules/auth/services/auth.service.spec.ts
+++ src/app/modules/auth/services/auth.service.spec.ts
@@ -1,21 +1,53 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-
+import * as mockRaw from '../../../data/user.json';
 import { AuthService } from './auth.service';
+import { CookieService } from 'ngx-cookie-service';
+import { of } from 'rxjs/internal/observable/of';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let mockUser: any = (mockRaw as any).default
+  let mockCookieService;
+  let httpClientSpy: { post: jasmine.Spy }
 
   beforeEach(() => {
+    mockCookieService = jasmine.createSpyObj('CookieService', ['get','check', 'set', 'delete']);
+    mockCookieService.check.and.returnValue(true);
     TestBed.configureTestingModule({
       imports: [
         HttpClientTestingModule
+      ],
+      providers: [
+        { provide: CookieService, useValue: mockCookieService }
       ]
     });
-    service = TestBed.inject(AuthService);
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['post'])
+    service = new AuthService(httpClientSpy as any, mockCookieService);
   });
 
-  it('should be created', () => {
+  it('component should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('Should return object with data and token session', 
+  (done: DoneFn) => {
+    const user: any = mockUser.userOk
+    const mockResponse = {
+      data: {},
+      tokenSession: '0x0x0x'
+    }
+
+    httpClientSpy.post.and.returnValue(
+      of(mockResponse)
+    )
+
+    service.sendCredentials(user.email, user.password)
+    .subscribe(responseApi => {
+      const getProperties = Object.keys(responseApi)
+      expect(getProperties).toContain('data')
+      expect(getProperties).toContain('tokenSession')
+      done()
+    })
   });
 });
